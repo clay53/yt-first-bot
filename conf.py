6663c7b658c67c4c313668061475ff9c0bf91fa4
clay53 <clayton.hickey@yahoo.com>
2018-03-07 01:13:22 -0500

    Completed version 0.1

---
+++ conf.py
@@ -0,0 +1,12 @@
+import json
+
+comment = "Hello."
+playlists = [
+    {
+        'key' : 'UUHVvQgQd4JrIS_FOFbHOK0g',
+        'last' : ''
+    }, {
+        'key' : 'UUXuqSBlHAE6Xw-yeJA0Tunw',
+        'last' : ''
+    }
+]
